fa909b5f71a6c03859165ed2405bfc4a09da6721
Edward Raigosa <edward.raigosa@snowflake.com>
2024-03-22 20:21:07 -0700

    Update v1_new_database.sql

--- migrations/v1_new_database.sql
+++ migrations/v1_new_database.sql
@@ -1,1 +1,8 @@
-
+-- helo world test, from this training
+-- https://quickstarts.snowflake.com/guide/devops_dcm_schemachange_github/index.html#3
+CREATE SCHEMA DEMO;
+CREATE TABLE HELLO_WORLD
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+);
