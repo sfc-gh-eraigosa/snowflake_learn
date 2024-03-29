22a03833360507842ecc6977ad45c3b5352ae775
Edward Raigosa <edward.raigosa@gmail.com>
2024-03-28 23:43:56 -0700

    test

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -1,8 +1,8 @@
 -- helo world test, from this training
 -- https://quickstarts.snowflake.com/guide/devops_dcm_schemachange_github/index.html#3
 -- works with ; https://github.com/Snowflake-Labs/schemachange?tab=readme-ov-file#repeatable-script-naming
-CREATE IF NOT EXISTS SCHEMA DEMO;
-CREATE TABLE IF NOT EXISTS HELLO_WORLD
+CREATE SCHEMA DEMO;
+CREATE TABLE HELLO_WORLD
 (
    FIRST_NAME VARCHAR
   ,LAST_NAME VARCHAR
